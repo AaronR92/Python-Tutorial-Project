5d4b376f8f587369f3666041a6dddb93beb283f3
Anton <86726028+AaronR92@users.noreply.github.com>
2023-03-10 23:04:37 +0300

    Logic updated. Now votes are saving in db.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 import pkgutil
 
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -23,9 +24,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
